6aebdb2eb8957f0ac57703e5f471848605a1853b
Rahdeg <walett95@gmail.com>
2022-07-26 19:28:32 +0100

    update

--- src/Component/Experience/experience.js
+++ src/Component/Experience/experience.js
@@ -12,10 +12,10 @@ const experience = () => {
                     <div class="role">
                         <h3 class="company-title2">Operations Officer</h3>
                         <ul>
-                            <li>Providing Online Registration of Exams for Individuals and Educational Bodies</li>
+                            <li>Maintaining Registration website using Reactjs</li>
                             <li>Provides excellent customer care to maintain and improve customer relations, strengthen loyalty and increase product and service needs.</li>
-                            <li>Contribute to business development efforts to drive revenue and meet business objectives.</li>
-                            <li>Handle administrative requirements smoothly and efficiently, including maintaining records and submitting daily report.</li>
+                            <li>Managing the Products Database efficiently</li>
+                            <li>Handle administrative requirements smoothly and efficiently</li>
                         </ul>
                     </div>
                 </div>
@@ -27,10 +27,9 @@ const experience = () => {
                     <div class="role">
                         <h3 class="company-title2">CAFE MANAGER</h3>
                         <ul>
-                        <li>Providing Online Registration of Exams for Individuals and Educational Bodies</li>
-                        <li>Provides excellent customer care to maintain and improve customer relations, strengthen loyalty and increase product and service needs.</li>
+                        <li>Developes and maintain websites for data storage and retrieval using React.js </li>
                         <li>Contribute to business development efforts to drive revenue and meet business objectives.</li>
-                        <li>Handle administrative requirements smoothly and efficiently, including maintaining records and submitting daily report.</li>
+                        <li>Handle administrative requirements smoothly and efficientl.</li>
                     </ul>
                     </div>
                 </div>
